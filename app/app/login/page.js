0b23b349209cf4fbb6afada9fa04a060ec025bde
av557649-eng <av557649@gmail.com>
2026-04-20 13:11:21 +0400

    Create page.js

---
+++ app/app/login/page.js
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { useRouter } from "next/navigation";
+
+export default function Login() {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      router.push("/dashboard");
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>Login</h2>
+
+      <input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+      <br /><br />
+
+      <input type="password" placeholder="password" onChange={(e) => setPassword(e.target.value)} />
+      <br /><br />
+
+      <button onClick={login}>Login</button>
+    </div>
+  );
+}
