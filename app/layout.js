4371170e06596dd8a8c459196fc5d65e6609a44f
av557649-eng <av557649@gmail.com>
2026-04-20 14:44:42 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body style={{ background: 'white', margin: 0 }}>
+    <html lang="en">
+      <body style={{ background: "white", margin: 0 }}>
         <header
           style={{
-            padding: '15px',
-            borderBottom: '1px solid #ddd',
-            color: '#2E3A63',
-            fontWeight: 'bold',
-            fontSize: '20px',
+            padding: "15px",
+            borderBottom: "1px solid #ddd",
+            color: "#2E3A63",
+            fontWeight: "bold",
+            fontSize: "20px",
           }}
         >
           AL SHIRAWI EQUIPMENT CO
