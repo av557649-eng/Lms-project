123d57c5fda643d0756c89cbed401781572ac517
av557649-eng <av557649@gmail.com>
2026-04-21 09:44:21 +0400

    Update page.js

--- app/course/[vehicle]/page.js
+++ app/course/[vehicle]/page.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import { db } from "@/lib/firebase";
+import { db } from "../../../lib/firebase";
 import { collection, getDocs } from "firebase/firestore";
 
 export default function CoursePage() {
