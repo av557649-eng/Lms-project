befd7fc815282128163fa273a5ab90f7391b514f
av557649-eng <av557649@gmail.com>
2026-04-21 12:29:26 +0400

    Create page.js

---
+++ app/lesson-view/[vehicle]/[module]/[lesson]/page.js
@@ -0,0 +1,58 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { db } from "../../../../lib/firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+export default function LessonView() {
+  const { vehicle, module, lesson } = useParams();
+  const router = useRouter();
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const load = async () => {
+      const ref = doc(
+        db,
+        "Courses",
+        vehicle,
+        "Modules",
+        module,
+        "Lesson",
+        lesson
+      );
+
+      const snap = await getDoc(ref);
+
+      if (snap.exists()) {
+        setData(snap.data());
+      }
+    };
+
+    if (vehicle && module && lesson) load();
+  }, [vehicle, module, lesson]);
+
+  if (!data) return <div>Loading video...</div>;
+
+  return (
+    <div style={{ padding: 40 }}>
+      <h2>{data.title}</h2>
+
+      <video width="600" controls>
+        <source src={data.videoUrl} />
+      </video>
+
+      <p>{data.description}</p>
+
+      <button
+        onClick={() =>
+          router.push(`/quiz/${vehicle}/${module}/${lesson}`)
+        }
+        style={{ marginTop: 20 }}
+      >
+        Start Quiz
+      </button>
+    </div>
+  );
+}
