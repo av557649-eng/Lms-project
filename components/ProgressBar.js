e1ff0ed3d9ab6228dc5b6c984c77956d4f2e1986
av557649-eng <av557649@gmail.com>
2026-04-24 13:57:02 +0400

    Update ProgressBar.js

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -1,1 +1,17 @@
+"use client";
 
+export default function ProgressBar({ value, total }) {
+  const percentage = total ? (value / total) * 100 : 0;
+
+  return (
+    <div style={{ width: "100%", background: "#eee", height: 20 }}>
+      <div
+        style={{
+          width: `${percentage}%`,
+          background: "green",
+          height: "100%"
+        }}
+      />
+    </div>
+  );
+}
