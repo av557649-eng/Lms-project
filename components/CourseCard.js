dbef860d1d7ba6cee7e5e2c937eb447aa3e9dc19
av557649-eng <av557649@gmail.com>
2026-04-24 13:56:12 +0400

    Update CourseCard.js

--- components/CourseCard.js
+++ components/CourseCard.js
@@ -1,1 +1,18 @@
+"use client";
 
+export default function CourseCard({ title, onClick }) {
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        padding: 15,
+        border: "1px solid black",
+        margin: 10,
+        cursor: "pointer",
+        borderRadius: 8
+      }}
+    >
+      <h3>{title}</h3>
+    </div>
+  );
+}
