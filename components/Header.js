6bda21a5958588d84bc6e19d5ce92e4962d9ca84
av557649-eng <av557649@gmail.com>
2026-04-24 13:56:27 +0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,19 @@
+"use client";
 
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: 20,
+        borderBottom: "1px solid #ccc"
+      }}
+    >
+      <Image src="/logo-left.png" alt="logo" width={100} height={50} />
+      <Image src="/logo-right.png" alt="logo" width={100} height={50} />
+    </div>
+  );
+}
